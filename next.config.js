70dec84484d1dfe09b1309c1c3513f2b55907866
Walid Nasim <wanasim92@gmail.com>
2024-07-29 17:48:01 -0400

    adding copyplugin for wasm module

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   webpack: (config, { isServer, dev }) => {
     config.experiments = {
       asyncWebAssembly: true,
       layers: true,
     };
+
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "./node_modules/tiktoken/lite/tiktoken_bg.wasm",
+          },
+        ],
+      }),
+    );
 
     return config;
   },
