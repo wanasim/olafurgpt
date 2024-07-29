884e8071e2a910bd439e0b51f4d1bcfe9bc3a176
Walid Nasim <wanasim92@gmail.com>
2024-07-29 18:05:27 -0400

    increment llamaindex & remove copyplugin ref

--- next.config.js
+++ next.config.js
@@ -7,15 +7,17 @@ module.exports = {
       layers: true,
     };
 
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: "./node_modules/tiktoken/lite/tiktoken_bg.wasm",
-          },
-        ],
-      }),
-    );
+    // config.plugins.push(
+    //   new CopyPlugin({
+    //     patterns: [
+    //       {
+    //         from: "./node_modules/tiktoken/lite/tiktoken_bg.wasm",
+    //         to: "tiktoken_bg.wasm",
+    //         toType: "file",
+    //       },
+    //     ],
+    //   }),
+    // );
 
     return config;
   },
